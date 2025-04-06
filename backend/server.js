1075040d5a1c51fb77d01f3d63db024a88ba091b
Adarsh Kumar Singh <adarshkumarsingh2612@gmail.com>
2025-04-06 13:30:03 +0530

    Changes in server.

--- backend/server.js
+++ backend/server.js
@@ -61,7 +61,7 @@ const io = new Server(server, {
     },
 });
 app.use(cors({
-    origin: 'https://hectoclash-eetqasp4a-singhadarsh2612s-projects.vercel.app',
+    origin: 'https://hectoclash-du0twjprv-singhadarsh2612s-projects.vercel.app',
     credentials: true,
   }));
   
